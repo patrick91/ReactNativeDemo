83ed998a0b63269b154af9d2a38095faae6481e2
Patrick Arminio <patrick.arminio@gmail.com>
2016-05-03 18:17:25 +0200

    Live demo code

--- index.ios.js
+++ index.ios.js
@@ -9,14 +9,40 @@ import React, {
     Component,
     StyleSheet,
     Text,
-    View
+    View,
+    TouchableOpacity,
+    Animated,
 } from 'react-native';
 
 class Meetup extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: 0,
+            opacityValue: new Animated.Value(0),
+        };
+
+        this.incrementCounter = this.incrementCounter.bind(this);
+    }
+
+    incrementCounter() {
+        this.setState({
+            count: this.state.count + 1,
+        });
+
+        const endValue = (this.state.count + 1) * 10 / 100;
+
+        Animated.timing(this.state.opacityValue, {
+            duration: 1000,
+            toValue: endValue,
+        }).start();
+    }
+
     render() {
         return <View style={styles.container}>
             <Text style={styles.welcome}>
-                Welcome to React Native!
+                Welcome to JS Meetup!
             </Text>
             <Text style={styles.instructions}>
                 To get started, edit index.ios.js
@@ -25,6 +51,25 @@ class Meetup extends Component {
                 Press Cmd+R to reload, {'\n'}
                 Cmd+D or shake for dev menu
             </Text>
+
+            <Text style={styles.counter}>
+                {this.state.count}
+            </Text>
+
+            <Animated.View style={{
+                backgroundColor: 'purple',
+                width: 100,
+                height: 100,
+                opacity: this.state.opacityValue,
+            }}>
+
+            </Animated.View>
+
+            <TouchableOpacity onPress={this.incrementCounter} style={styles.button}>
+                <Text style={styles.buttonText}>
+                    Click me
+                </Text>
+            </TouchableOpacity>
         </View>;
     }
 }
@@ -46,6 +91,16 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    button: {
+        backgroundColor: 'black',
+        padding: 20,
+    },
+    buttonText: {
+        color: 'white',
+    },
+    counter: {
+        fontSize: 40,
+    }
 });
 
 AppRegistry.registerComponent('Meetup', () => Meetup);
